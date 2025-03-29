453c3563b0599a27550a71734d828b85527219be
ivaquero <msaintms@outlook.com>
2025-03-29 13:40:58 +0800

    format

--- .scripts/run_osx_build.sh
+++ .scripts/run_osx_build.sh
@@ -10,7 +10,7 @@ MINIFORGE_HOME="${MINIFORGE_HOME:-${HOME}/miniforge3}"
 MINIFORGE_HOME="${MINIFORGE_HOME%/}" # remove trailing slash
 export CONDA_BLD_PATH="${CONDA_BLD_PATH:-${MINIFORGE_HOME}/conda-bld}"
 
-( startgroup "Provisioning base env with micromamba" ) 2> /dev/null
+(startgroup "Provisioning base env with micromamba") 2>/dev/null
 MICROMAMBA_VERSION="1.5.10-0"
 if [[ "$(uname -m)" == "arm64" ]]; then
   osx_arch="osx-arm64"
@@ -31,18 +31,14 @@ echo "Moving pkgs cache from ${MAMBA_ROOT_PREFIX} to ${MINIFORGE_HOME}"
 mv "${MAMBA_ROOT_PREFIX}/pkgs" "${MINIFORGE_HOME}"
 echo "Cleaning up micromamba"
 rm -rf "${MAMBA_ROOT_PREFIX}" "${micromamba_exe}" || true
-( endgroup "Provisioning base env with micromamba" ) 2> /dev/null
+(endgroup "Provisioning base env with micromamba") 2>/dev/null
 
-( startgroup "Configuring conda" ) 2> /dev/null
+(startgroup "Configuring conda") 2>/dev/null
 echo "Activating environment"
 source "${MINIFORGE_HOME}/etc/profile.d/conda.sh"
 conda activate base
 export CONDA_SOLVER="libmamba"
 export CONDA_LIBMAMBA_SOLVER_NO_CHANNELS_FROM_INSTALLED=1
-
-
-
-
 
 echo -e "\n\nSetting up the condarc and mangling the compiler."
 setup_conda_rc ./ ./recipe ./.ci_support/${CONFIG}.yaml
@@ -66,9 +62,7 @@ fi
 echo -e "\n\nRunning the build setup script."
 source run_conda_forge_build_setup
 
-
-
-( endgroup "Configuring conda" ) 2> /dev/null
+(endgroup "Configuring conda") 2>/dev/null
 
 echo -e "\n\nMaking the build clobber file"
 make_build_number ./ ./recipe ./.ci_support/${CONFIG}.yaml
@@ -78,43 +72,43 @@ if [[ -f LICENSE.txt ]]; then
 fi
 
 if [[ "${BUILD_WITH_CONDA_DEBUG:-0}" == 1 ]]; then
-    if [[ "x${BUILD_OUTPUT_ID:-}" != "x" ]]; then
-        EXTRA_CB_OPTIONS="${EXTRA_CB_OPTIONS:-} --output-id ${BUILD_OUTPUT_ID}"
-    fi
-    conda debug ./recipe -m ./.ci_support/${CONFIG}.yaml \
-        ${EXTRA_CB_OPTIONS:-} \
-        --clobber-file ./.ci_support/clobber_${CONFIG}.yaml
+  if [[ "x${BUILD_OUTPUT_ID:-}" != "x" ]]; then
+    EXTRA_CB_OPTIONS="${EXTRA_CB_OPTIONS:-} --output-id ${BUILD_OUTPUT_ID}"
+  fi
+  conda debug ./recipe -m ./.ci_support/${CONFIG}.yaml \
+    ${EXTRA_CB_OPTIONS:-} \
+    --clobber-file ./.ci_support/clobber_${CONFIG}.yaml
 
-    # Drop into an interactive shell
-    /bin/bash
+  # Drop into an interactive shell
+  /bin/bash
 else
 
-    if [[ "${HOST_PLATFORM}" != "${BUILD_PLATFORM}" ]]; then
-        EXTRA_CB_OPTIONS="${EXTRA_CB_OPTIONS:-} --no-test"
-    fi
+  if [[ "${HOST_PLATFORM}" != "${BUILD_PLATFORM}" ]]; then
+    EXTRA_CB_OPTIONS="${EXTRA_CB_OPTIONS:-} --no-test"
+  fi
 
-    conda-build ./recipe -m ./.ci_support/${CONFIG}.yaml \
-        --suppress-variables ${EXTRA_CB_OPTIONS:-} \
-        --clobber-file ./.ci_support/clobber_${CONFIG}.yaml \
-        --extra-meta flow_run_id="$flow_run_id" remote_url="$remote_url" sha="$sha"
+  conda-build ./recipe -m ./.ci_support/${CONFIG}.yaml \
+    --suppress-variables ${EXTRA_CB_OPTIONS:-} \
+    --clobber-file ./.ci_support/clobber_${CONFIG}.yaml \
+    --extra-meta flow_run_id="$flow_run_id" remote_url="$remote_url" sha="$sha"
 
-    ( startgroup "Inspecting artifacts" ) 2> /dev/null
+  (startgroup "Inspecting artifacts") 2>/dev/null
 
-    # inspect_artifacts was only added in conda-forge-ci-setup 4.9.4
-    command -v inspect_artifacts >/dev/null 2>&1 && inspect_artifacts --recipe-dir ./recipe -m ./.ci_support/${CONFIG}.yaml || echo "inspect_artifacts needs conda-forge-ci-setup >=4.9.4"
+  # inspect_artifacts was only added in conda-forge-ci-setup 4.9.4
+  command -v inspect_artifacts >/dev/null 2>&1 && inspect_artifacts --recipe-dir ./recipe -m ./.ci_support/${CONFIG}.yaml || echo "inspect_artifacts needs conda-forge-ci-setup >=4.9.4"
 
-    ( endgroup "Inspecting artifacts" ) 2> /dev/null
-    ( startgroup "Validating outputs" ) 2> /dev/null
+  (endgroup "Inspecting artifacts") 2>/dev/null
+  (startgroup "Validating outputs") 2>/dev/null
 
-    validate_recipe_outputs "${FEEDSTOCK_NAME}"
+  validate_recipe_outputs "${FEEDSTOCK_NAME}"
 
-    ( endgroup "Validating outputs" ) 2> /dev/null
+  (endgroup "Validating outputs") 2>/dev/null
 
-    ( startgroup "Uploading packages" ) 2> /dev/null
+  (startgroup "Uploading packages") 2>/dev/null
 
-    if [[ "${UPLOAD_PACKAGES}" != "False" ]] && [[ "${IS_PR_BUILD}" == "False" ]]; then
-      upload_package --validate --feedstock-name="${FEEDSTOCK_NAME}" ./ ./recipe ./.ci_support/${CONFIG}.yaml
-    fi
+  if [[ "${UPLOAD_PACKAGES}" != "False" ]] && [[ "${IS_PR_BUILD}" == "False" ]]; then
+    upload_package --validate --feedstock-name="${FEEDSTOCK_NAME}" ./ ./recipe ./.ci_support/${CONFIG}.yaml
+  fi
 
-    ( endgroup "Uploading packages" ) 2> /dev/null
+  (endgroup "Uploading packages") 2>/dev/null
 fi
